a3ca369bf1a2164c470cf54338757da5d3a9f032
Maryam <maryamolabisi02@gmail.com>
2015-11-04 14:25:21 +0100

    feat(src): Include stringClass.js file

---
+++ jasmine/src/stringClass.js
@@ -0,0 +1,78 @@
+/********************************************************
+*Name: Inverted Index.
+*Description:  Extending the String Class (Checkpoint Two)
+*Author: Babalola Maryam
+****************************************************/
+
+/**
+ * [function to check weather a string contains vowels or not]
+ * @return {[boolean]} [returns true if a vowel is found else false]
+ */
+String.prototype.hasVowels = function() {
+    var regex = /[aeiou]/;
+    return regex.test(this);
+};
+
+/**
+ * [function to convert a string to uppercase]
+ * @return {[string]} [returns a string with all its characters in upper case]
+ */
+String.prototype.toUpper = function() {
+
+};
+
+/**
+ * [function to convert a string to lowercase]
+ * @return {[string]} [returns a string with all its characters in lower case]
+ */
+String.prototype.toLower = function() {
+
+};
+
+/**
+ * [function to convert the first character of a string to uppercase]
+ * @return {[string]} [returns a string with its first character in upper case]
+ */
+String.prototype.ucFirst = function() {
+
+};
+
+/**
+ * [function to check if the string is a question]
+ * @return {[boolean]} [returns true if the string ends with a question mark]
+ */
+StringingClass.prototype.isQuestion = function() {
+
+};
+
+/**
+ * [function to return a list of the words in a string]
+ * @return {[array]} [returns a list of the words in a string as an array]
+ */
+String.prototype.words = function() {
+
+};
+
+/**
+ * [function to return the number of words in a string]
+ * @return {[number]} [returns the number of words in a string]
+ */
+String.prototype.wordCount = function() {
+
+};
+
+/**
+ * [function to return a currency representation of a string]
+ * @return {[string]} [returns the number of words in a stringp]
+ */
+String.prototype.toCurrency = function() {
+
+};
+
+/**
+ * [function to return a number representation of a currency string]
+ * @return {[number]} [returns a number representation of a currency string]
+ */
+String.prototype.fromCurrency = function() {
+
+};
